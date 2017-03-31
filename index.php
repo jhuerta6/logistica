fe1a3af9ed49dabf9c690285c96b4e4b44b341d7
jhuerta6 <jorgehuertahu@gmail.com>
2017-03-30 23:36:57 -0600

    Pagina para logistica aduanera juarez, primer commit. Por hacer: video.

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Logistica Aduanera Juarez</title>
+
+  <!-- Bootstrap -->
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.js"></script>
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/custom.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Logistica Aduanera</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#">INICIO</a></li>
+        <li id="custom_hover_nav"><a class="dropdown-toggle" data-toggle="dropdown" href="#">INVENTARIO EN SUBASTA <span class="caret"></span></a>
+          <ul class="dropdown-menu" id="collapse">
+            <li><a href="#">Harley Davidson</a></li>
+            <li><a href="#">Motos 2</a></li>
+            <li><a href="#">Motos 3</a></li>
+          </ul></li>
+          <li><a href="#">TERMINOS Y CONDICIONES</a></li>
+          <li><a href="#">CONTACTANOS</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registrate</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <script>
+    $(document).ready(function(){
+      $("#custom_hover_nav").hover(function(){ //show inventario on hover
+        $("#collapse").collapse('toggle');
+    });
+    });
+  </script>
+
+  <!--<h1>Hello, world!</h1>-->
+
+</body>
